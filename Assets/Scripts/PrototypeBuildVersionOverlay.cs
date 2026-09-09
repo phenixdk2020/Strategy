25e6a88c7b86b1ea2ec3be92129fc18d88354c67
phenixdk2020 <74060601+phenixdk2020@users.noreply.github.com>
2026-09-09 06:33:13 +0200

    Bump tactical TEST to v00.00.09i

--- Assets/Scripts/PrototypeBuildVersionOverlay.cs
+++ Assets/Scripts/PrototypeBuildVersionOverlay.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [DefaultExecutionOrder(20000)]
 public sealed class PrototypeBuildVersionOverlay : MonoBehaviour
 {
-    public const string BuildVersion = "v00.00.09h4";
+    public const string BuildVersion = "v00.00.09i";
     public const string BuildChannel = "TEST";
 
     private GUIStyle style;
