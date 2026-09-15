40fec5c02ecbc5662d49e03b288f8bcbd32f6f4c
phenixdk2020 <74060601+phenixdk2020@users.noreply.github.com>
2026-09-15 12:08:10 +0200

    v00.00.09f29b bump visible test marker

--- Assets/Scripts/PrototypeBuildVersionOverlay.cs
+++ Assets/Scripts/PrototypeBuildVersionOverlay.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [DefaultExecutionOrder(20000)]
 public sealed class PrototypeBuildVersionOverlay : MonoBehaviour
 {
-    public const string BuildVersion = "v00.00.09f29a";
+    public const string BuildVersion = "v00.00.09f29b";
     public const string BuildChannel = "TEST";
 
     private GUIStyle style;
