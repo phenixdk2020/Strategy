7781df77387e66dd1a07951923d0161643769289
phenixdk2020 <74060601+phenixdk2020@users.noreply.github.com>
2026-09-20 09:41:52 +0200

    v00.00.09f30r update higher HUD build caption

--- Assets/Scripts/PrototypeUnifiedCommandHud09F29G.cs
+++ Assets/Scripts/PrototypeUnifiedCommandHud09F29G.cs
@@ -169,8 +169,8 @@ public sealed class PrototypeUnifiedCommandHud09F29G : MonoBehaviour
         bool division =
             higher.SelectedLevel == PrototypeHigherCommandLevel09F30B.Division;
         string title = division
-            ? "1. DIVISION | DIVISIONSCHEF | HØJERE KOMMANDO | F30Q"
-            : "1. BRIGADE | BRIGADECHEF | HØJERE KOMMANDO | F30Q";
+            ? "1. DIVISION | DIVISIONSCHEF | HØJERE KOMMANDO | F30R"
+            : "1. BRIGADE | BRIGADECHEF | HØJERE KOMMANDO | F30R";
 
         GUI.Box(
             new Rect(5f, panel.y + 3f, panel.width - 10f, 17f),
